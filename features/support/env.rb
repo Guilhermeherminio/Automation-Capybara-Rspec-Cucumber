e7e17ee731e54d78ef38c6bbf8205f626775f7e8
Sousa <guilherme.sousa@hp.com>
2021-03-19 23:15:56 -0300

    update git ignore and some gems to remove that

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,7 +2,6 @@ require 'capybara/dsl'
 require 'capybara/rspec/matchers'
 require 'cucumber'
 require 'selenium-webdriver'
-require 'report_builder'
 require 'rspec'
 require 'rubocop'
 require 'faraday'
@@ -10,7 +9,6 @@ require 'byebug'
 require 'site_prism'
 require 'capybara/cucumber'
 require 'capybara-screenshot/cucumber'
-require 'base64'
 
 Capybara.default_driver = :selenium_chrome 
 Capybara.default_max_wait_time = 20
